2becffb93527bf8fbc349b0c63fc7b29e728e6e7
karenqh <heqin@seas.upenn.edu>
2012-11-19 14:59:15 -0500

    updated the svm training function and run submission frame

--- code/run_submission.m
+++ code/run_submission.m
@@ -1,10 +1,10 @@
 clear;
 % pack;
 load music_dataset.mat 
-load Xtest.mat 
-load Xtrain.mat 
-load Ytest.mat
-load Ytrain.mat
+% load Xtest.mat 
+% load Xtrain.mat 
+% load Ytest.mat
+% load Ytrain.mat
 
 [Xt_lyrics] = make_lyrics_sparse(train, vocab);
 [Xq_lyrics] = make_lyrics_sparse(quiz, vocab);
@@ -47,24 +47,26 @@ end
 % save('Ytest_audio.mat', 'Ytest_audio');
 
 
-[test_err info] = train_svm(Xtrain, Ytrain);
+% [train_err info] = train_svm(Xtrain, Ytrain);
+test_svm(Xtrain, Ytrain, Xtest, Ytest);
 
-%% "Train" just the intersection kernal
-best_loop_size = just_kernel_train(Xtrain, Ytrain);
-% It turns out that the best loop size is exactly 10...
+% %% "Train" just the intersection kernal
+% best_loop_size = just_kernel_train(Xtrain, Ytrain);
+% % It turns out that the best loop size is exactly 10...
 
 %% Run algorithm
-% ranks = predict_genre(Xtrain, Xtest, Ytrain);
-ranks = predict_genre(Xtrain_audio, Xtest_audio, Ytrain_audio);
+Yq = ones(size(Xq_lyrics, 1), 1);
+[test_error ranks info] = test_svm(Xt_lyrics, Yt, Xq_lyrics, Yq);
+% ranks = predict_genre(Xtrain_audio, Xtest_audio, Ytrain_audio);
 
 % Got the rank loss
-result = rank_loss(ranks, Ytest_audio)
+% result = rank_loss(ranks, Ytest_audio)
 
 % %% Generate submissions
 % ranks = predict_genre(Xt_lyrics, Xq_lyrics, Yt);
 % %                       Xt_audio, Xq_audio, ...
 % %                       Yt);
 % 
-% % Save results to a text file for submission
-% save('-ascii', 'submit.txt', 'ranks');
+% Save results to a text file for submission
+save('-ascii', 'submit.txt', 'ranks');
 
--- code/train_svm.m
+++ code/train_svm.m
@@ -5,31 +5,32 @@ Xtrain = bsxfun(@rdivide, Xtrain, sum(Xtrain, 2));
 % Xtest = bsxfun(@rdivide, Xtest, sum(Xtest, 2));
 
 % Cross validation to get an average error
-n_fold = 10;
-partitions = make_xval_partition(size(Xtrain, 1), n_fold);
-xval_train_set = Xtrain(partitions~=n_fold, :);
-xval_train_label = Ytrain(partitions~=n_fold);
-xval_test_set = Xtrain(partitions==n_fold, :);
-xval_test_label = Ytrain(partitions==n_fold);
+for cv = 1:3
+    
+    n_fold = 10;
+    partitions = make_xval_partition(size(Xtrain, 1), n_fold);
+    xval_train_set = Xtrain(partitions~=n_fold, :);
+    xval_train_label = Ytrain(partitions~=n_fold);
+    xval_test_set = Xtrain(partitions==n_fold, :);
+    xval_test_label = Ytrain(partitions==n_fold);
 
-% scores = zeros(size(xval_test_set, 10));
+    % scores = zeros(size(xval_test_set, 10));
 
 
-% Use kerneled SVM to train. We need probability predictions
-% kernel = @(x, x2) kernel_intersection(x, x2);
-kernel = @(x, x2) kernel_poly(x, x2, 1);
+    % Use kerneled SVM to train. We need probability predictions
+    kernel = @(x, x2) kernel_intersection(x, x2);
+    % kernel = @(x, x2) kernel_poly(x, x2, 1);
 
-[xval_err info] = kernel_libsvm(xval_train_set, xval_train_label,...
-                                xval_test_set, xval_test_label, kernel);
-display(num2str(xval_err));
+    [xval_err info] = kernel_libsvm(xval_train_set, xval_train_label,...
+                                    xval_test_set, xval_test_label, kernel);
+    display(num2str(xval_err));
 
-scores = zeros(size(xval_test_set,1), 10);
-label_seq = (info.model.Label)';
-for i = 1:size(xval_test_set,1)
-    scores(i, label_seq) = info.vals(i, :);
-%     for j = 1:10
-%         scores(i, info.model.Label(j)) = info.vals(i, j);
-%     end
+    scores = zeros(size(xval_test_set,1), 10);
+    label_seq = (info.model.Label)';
+    for i = 1:size(xval_test_set,1)
+        scores(i, label_seq) = info.vals(i, :);
+    end
+    rank = get_ranks(scores);
+    loss = rank_loss(rank, xval_test_label)
+    
 end
-rank = get_ranks(scores);
-loss = rank_loss(rank, xval_test_label)
